63f59af1107bd94e1bc4a32cdd09acbef156a9df
pabloserra89 <pabloserra89@gmail.com>
2013-08-27 23:58:57 -0300

    Cree el Activity para los datos personales. Está pelado (sin ofender =P) todavía.

--- src/com/proyecto/bav/MainActivity.java
+++ src/com/proyecto/bav/MainActivity.java
@@ -25,25 +25,36 @@ public class MainActivity extends BaseSpiceActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    // Handle item selection
 	    switch (item.getItemId()) {
-	        case R.id.show_addresses:
+	    	case R.id.btn_datos_personales:
+	    		showDatosPersonales();
+	    		return true;
+	    	case R.id.menu_datos_personales:
+	    		showDatosPersonales();
+	    		return true;
+	        case R.id.btn_show_addresses:
 	        	showAddresses();
 	            return true;
-	       /* case R.id.help:
-	            showHelp();
-	            return true;*/
+	        case R.id.menu_show_addresses:
+	        	showAddresses();
+	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
 	
-	/** Called when the user clicks the Denunciar button */
-	public void enviarDenuncia(View view) {
-		
-	}
+	private void showDatosPersonales() {
+		Intent intent = new Intent(this, DatosPersonalesActivity.class);
+		startActivity(intent);
+	}	
 
 	private void showAddresses() {
 		Intent intent = new Intent(this, DisplayAddressesActivity.class);
 		startActivity(intent);
+	}
+	
+	/** Called when the user clicks the Denunciar button */
+	public void enviarDenuncia(View view) {
+		
 	}
 
 }
