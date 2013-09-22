d0713ed6420ee7d59f50d836c3c64a08ce72ba52
pabloserra89 <pabloserra89@gmail.com>
2013-09-22 18:18:00 -0300

    Pantalla de Login mejorada

--- src/com/proyecto/bav/LoginActivity.java
+++ src/com/proyecto/bav/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.proyecto.bav;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends BaseSpiceActivity {
 	
@@ -56,7 +59,16 @@ public class LoginActivity extends BaseSpiceActivity {
 	
 	/** Called when the user clicks the olvidePass button */
 	public void olvidePass(View view) {
-
+		
+		EditText editTextEmail = (EditText) findViewById(R.id.et_email);
+		String email = editTextEmail.getText().toString();
+		
+		if(email.length() == 0)
+			Toast.makeText(getApplicationContext(), "Ingrese el e-mail", Toast.LENGTH_SHORT).show();
+		else if (!email.contains("@"))
+			Toast.makeText(getApplicationContext(), "Ingrese un e-mail válido", Toast.LENGTH_SHORT).show();
+		else
+			Toast.makeText(getApplicationContext(), "Olvidé mi contraseña", Toast.LENGTH_SHORT).show();
 		
 	}
 	
