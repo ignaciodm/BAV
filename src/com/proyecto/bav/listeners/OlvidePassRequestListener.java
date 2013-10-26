29a349202b856a858c9d3bce899ccaeddd25665d
pabloserra89 <pabloserra89@gmail.com>
2013-10-26 17:31:36 -0300

    Cambios generales.

--- src/com/proyecto/bav/listeners/OlvidePassRequestListener.java
+++ src/com/proyecto/bav/listeners/OlvidePassRequestListener.java
@@ -1,5 +1,9 @@
 package com.proyecto.bav.listeners;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.api.client.http.HttpResponseException;
 import com.octo.android.robospice.exception.NoNetworkException;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
@@ -20,9 +24,49 @@ public class OlvidePassRequestListener implements RequestListener<OlvidePassResu
 		activity.myProgressDialog.dismiss();
 		
 		if (spiceException instanceof NoNetworkException)
-			Dialog.showDialog(activity, false, true, "No hay conexión. Intente nuevamente");
+			Dialog.showDialog(activity, false, true, "No hay conexión. Intente nuevamente");		
+		else if (spiceException.getCause() instanceof HttpResponseException)
+			Dialog.showDialog(activity, false, true, this.getMessage(spiceException));		
 		else 
 			Dialog.showDialog(activity, false, true, "Ha ocurrido un error con la conexión. Intente nuevamente");
+	}
+
+	private String getMessage(SpiceException spiceException) {
+		
+		String stringException = spiceException.getCause().toString();
+		String json = stringException.substring(stringException.indexOf("{"), stringException.length());
+		
+		JSONObject jObj = null;
+		try {
+			jObj = new JSONObject(json);
+		} catch (JSONException e) {
+			return "Error. Intente nuevamente";
+		}
+		
+		String email = "";
+		try {
+			email = jObj.getString("email");
+		} catch (JSONException e) {
+			return "Error. Intente nuevamente";
+		}
+		
+		if(email != null)
+			return email;
+		else{
+			
+			String error = "";
+			try {
+				error = jObj.getString("email");
+			} catch (JSONException e) {
+				return "Error. Intente nuevamente";
+			}
+			
+			if(error != null)
+				return error;
+		}
+		
+		return "Error. Intente nuevamente";
+		
 	}
 
 	@Override
